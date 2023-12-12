1464571587e5acd204ba5d80a02f08a683c06173
Maheswari Kumar <ksrimahi@gmail.com>
2023-12-12 22:36:52 +0530

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import ViewCardOutlineIcon from "./icons/ViewCardOutlineIcon";
 import CaretDownOutlineIcon from "./icons/CaretDownOutlineIcon";
 import UpvoteOutlineIcon from "./icons/UpvoteOutlineIcon";
@@ -10,10 +10,49 @@ import ShareIOSOutlineIcon from "./icons/ShareIOSOutlineIcon";
 import Community from "./Community";
 import ViewClassicFillIcon from "./icons/ViewClassicFillIcon";
 import ViewCardFillIcon from "./icons/ViewCardFillIcon";
+import ViewClassicOutlineIcon from "./icons/ViewClassicOutlineIcon";
 
 export default function Home(){
     const [info, setInfo] = useState([]);
     const [limit, setLimit] = useState(10);
+
+    const IntialState = {
+      showDropDown : false,
+      showMin : true,
+      showMax : false,
+    }
+
+    function StateReducer(state, action){
+      switch(action.type) {
+        case "showoption" :
+          return {
+            ...state,
+            showDropDown : !state.showDropDown
+          }
+
+        case "showminoption" :
+          return {
+            ...state,
+            showMin : true,
+            showMax : false,
+            showDropDown : false
+          }
+
+          case "showmaxoption" :
+            return {
+              ...state,
+              showMax : true,
+              showMin : false,
+              showDropDown : false
+            }
+
+        default:
+          return state
+      }
+    }
+
+    const [state, StateDisptch] = useReducer(StateReducer, IntialState);
+    
 
     async function getData(){
         const rs = await axios.get(`https://academics.newtonschool.co/api/v1/reddit/post?limit=${limit}`, {
@@ -25,13 +64,10 @@ export default function Home(){
         console.log(rs.data.data);
     }
 
-
-
     useEffect(()=>{
         if (limit <= 100) {
             getData();
         }
-
     }, [limit])
 
     useEffect(()=>{
@@ -52,29 +88,31 @@ export default function Home(){
     return (
       <>
         <div className="p-1 mt-12 flex justify-center w-full max-w-full">
-        <div className="z-15 absolute right-[38rem] top-[7rem]">
-                <nav>View</nav>
-                <div>
-                  <div>
-                    <ViewCardFillIcon />
-                    <nav>Card</nav>
-                  </div>
-                  <div>
-                    <ViewClassicFillIcon />
-                    <nav>Classic</nav>
-                  </div>
-                </div>
-              </div>
-          <div className="flex justify-center w-full max-w-2xl flex-col m-2">
+          <div className={`flex justify-center w-full flex-col m-2 ${state.showMax ? "max-w-5xl" : "max-w-2xl"}`}>
             <div className="flex items-center pt-4 pb-4 justify-between gap-15 pb-7">
               <button className="border p-2 pr-3 pl-3 rounded-full border-black text-sm font-semibold cursor-pointer">
                 Create a Post
               </button>
-              <div className="flex items-center gap-1 hover:bg-gray-200 max-w-2xl rounded-full p-2 cursor-pointer">
-                <ViewCardOutlineIcon />
+              <div onClick={()=> StateDisptch({type:"showoption"})} className={`flex items-center gap-1 max-w-2xl rounded-full p-2 cursor-pointer ${state.showDropDown ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                {state.showMin ? <ViewCardOutlineIcon /> : <ViewClassicOutlineIcon /> }
                 <CaretDownOutlineIcon />
               </div>
             </div>
+            {state.showDropDown && <>
+            <div className={`z-10 fixed top-[7.5rem] shadow bg-white w-1/12 rounded-sm ${state.showMin ? "right-[35.5rem]" : "right-[25rem]"}`}>
+                <nav className="p-3 pl-4 pb-2">View</nav>
+                <div>
+                  <div onClick={()=> StateDisptch({type:"showminoption"})} className={`flex items-center gap-2 p-2 pl-6 h-12 cursor-pointer ${state.showMin ? "bg-gray-200" : "hover:bg-gray-100"}`}>
+                    {state.showMin ? <ViewCardFillIcon /> : <ViewCardOutlineIcon /> }
+                    <nav>Card</nav>
+                  </div> 
+                  <div onClick={()=> StateDisptch({type:"showmaxoption"})} className={`flex items-center gap-2 p-2 pl-6 h-12 cursor-pointer ${state.showMax ? "bg-gray-200" : "hover:bg-gray-100"}`}>
+                    {state.showMax ? <ViewClassicFillIcon /> : <ViewClassicOutlineIcon /> }
+                    <nav>Classic</nav>
+                  </div>
+                </div>
+              </div>
+          </>}
             {info.map((data, idx) => (
               <div
                 className="cursor-pointer m-2 border-b border-gray-300 pt-2 hover:bg-gray-200 p-2 rounded-lg"
